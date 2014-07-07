0c066dd05c42515ff3d1cb2e1999dfa000e67f91
Levi Thomason <me@levithomason.com>
2014-07-06 17:24:45 -0700

    changing name works!

--- js/app.js
+++ js/app.js
@@ -2,4 +2,13 @@ var app = angular.module('HackathonApp', []);
 
 app.controller('HackathonCtl', ['$scope', function($scope) {
 
+    $scope.init = function() {
+        $scope.my_name = null;
+        $scope.my_saved_name = 'World';
+    };
+
+    $scope.save_my_name = function() {
+        $scope.my_saved_name = $scope.my_name;
+    };
+
 }]);
