6bb18d8a6d7dee9ddcc355410fd23d341ce9f8b7
Levi Thomason <me@levithomason.com>
2014-07-06 18:20:19 -0700

    chat working

--- js/app.js
+++ js/app.js
@@ -4,14 +4,23 @@ app.controller('HackathonCtl', ['$scope', '$firebase', function($scope) {
 
     $scope.init = function() {
         // Firebase
-        var my_chat_ref = new Firebase('https://glowing-fire-2233.firebaseio.com/');
+        var my_chat_ref = new Firebase('https://glowing-fire-2233.firebaseio.com/my_chats');
 
-        $scope.my_name = null;
+
+        $scope.my_name = '';
         $scope.my_saved_name = 'World';
+        $scope.my_chat = '';
+        $scope.my_chats = [];
+
     };
 
     $scope.save_my_name = function() {
         $scope.my_saved_name = $scope.my_name;
+    };
+
+    $scope.save_my_chat = function() {
+        $scope.my_chats.push($scope.my_chat);
+        $scope.my_chat = '';
     };
 
 }]);
