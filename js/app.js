8d957374091ae0b5bd0b8631e166ccd2bc528827
Levi Thomason <me@levithomason.com>
2014-07-06 18:26:24 -0700

    could it be that easy?

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 var app = angular.module('HackathonApp', ['firebase']);
 
-app.controller('HackathonCtl', ['$scope', '$firebase', function($scope) {
+app.controller('HackathonCtl', ['$scope', '$firebase', function($scope, $firebase) {
 
     $scope.init = function() {
         // Firebase
         var my_chat_ref = new Firebase('https://glowing-fire-2233.firebaseio.com/my_chats');
 
-
         $scope.my_name = '';
         $scope.my_saved_name = 'World';
         $scope.my_chat = '';
-        $scope.my_chats = [];
+        $scope.my_chats = $firebase(my_chat_ref);
 
     };
 
@@ -19,7 +18,7 @@ app.controller('HackathonCtl', ['$scope', '$firebase', function($scope) {
     };
 
     $scope.save_my_chat = function() {
-        $scope.my_chats.push($scope.my_chat);
+        $scope.my_chats.$add($scope.my_chat);
         $scope.my_chat = '';
     };
 
