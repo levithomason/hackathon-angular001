21b8e6498b161c0ce2680d308c22f53eb6a8d846
Levi Thomason <me@levithomason.com>
2014-07-06 21:14:57 -0700

    adds chat with rooms

--- js/app.js
+++ js/app.js
@@ -7,19 +7,46 @@ app.controller('HackathonCtl', ['$scope', '$firebase', function($scope, $firebas
         var my_chat_ref = new Firebase('https://glowing-fire-2233.firebaseio.com/my_chats');
 
         $scope.my_name = '';
-        $scope.my_saved_name = 'World';
+        $scope.my_saved_name = 'Friend';
         $scope.my_chat = '';
         $scope.my_chats = $firebase(my_chat_ref);
-
+        $scope.my_name_is_saved = false;
+        $scope.my_channels = [
+            'Lobby',
+            'Among Friends',
+            'Hover to delete'
+        ];
+        $scope.my_channel = $scope.my_channels[0];
+        $scope.my_new_channel = '';
     };
 
-    $scope.save_my_name = function() {
+    $scope.send_my_name = function() {
         $scope.my_saved_name = $scope.my_name;
+        $scope.my_name_is_saved = true;
     };
 
-    $scope.save_my_chat = function() {
-        $scope.my_chats.$add($scope.my_chat);
+    $scope.send_my_chat = function() {
+        $scope.my_chats.$add({
+            user: $scope.my_saved_name,
+            channel: $scope.my_channel,
+            message: $scope.my_chat
+        });
         $scope.my_chat = '';
+    };
+
+    $scope.set_my_channel = function(channel) {
+        $scope.my_channel = channel;
+    };
+
+    $scope.delete_my_channel = function(channel) {
+        var index = $scope.my_channels.indexOf(channel);
+        $scope.my_channels.splice(index);
+    };
+
+    $scope.create_new_channel = function() {
+        $scope.my_channels.push($scope.my_new_channel);
+        $scope.my_channel = $scope.my_new_channel;
+        $scope.my_new_channel = '';
     };
 
 }]);
