b561b9542a0fb7c593abde6fd62d12f8c99e5da8
Levi Thomason <me@levithomason.com>
2014-07-06 17:50:37 -0700

    drops in firebase, not wired

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-var app = angular.module('HackathonApp', []);
+var app = angular.module('HackathonApp', ['firebase']);
 
-app.controller('HackathonCtl', ['$scope', function($scope) {
+app.controller('HackathonCtl', ['$scope', '$firebase', function($scope) {
 
     $scope.init = function() {
+        // Firebase
+        var my_chat_ref = new Firebase('https://glowing-fire-2233.firebaseio.com/');
+
         $scope.my_name = null;
         $scope.my_saved_name = 'World';
     };
